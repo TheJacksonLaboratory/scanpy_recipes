05c5d9be7ee7f99b1128d31a09ffd7079e6d0735
Bill Flynn <wflynny@gmail.com>
2018-12-11 15:50:22 -0500

    got qc and preprocessing to a good place

--- scanpy_recipes/recipes/preprocess.py
+++ scanpy_recipes/recipes/preprocess.py
@@ -1,45 +1,62 @@
 import os
 import numpy as np
 import pandas as pd
+from scanpy.preprocessing import simple as pp
+from scanpy.preprocessing.bbknn import bbknn
+from scanpy.neighbors import neighbors
+from scanpy.tools.umap import umap
+from scanpy.tools.leiden import leiden
+from scanpy.tools.louvain import louvain
 
 
 def preprocess(adata_raw, n_top_genes=1000, scale=False):
-    adata = sc.pp.normalize_per_cell(adata_raw, copy=True)
-    adata.uns['raw_dtype'] = 'normalized count'
+    adata = pp.normalize_per_cell(adata_raw, copy=True)
+    adata.uns["raw_dtype"] = "normalized count"
     adata.raw = adata
 
-    adata.uns['n_top_genes'] = n_top_genes
-    adata_filt = sc.pp.filter_genes_dispersion(adata, n_top_genes=n_top_genes,
-                                               flavor='cell_ranger', log=True, copy=True)
-    sc.pp.normalize_per_cell(adata)
+    adata.uns["n_top_genes"] = n_top_genes
+    adata_filt = pp.filter_genes_dispersion(adata, n_top_genes=n_top_genes,
+                                               flavor="cell_ranger", log=True, copy=True)
+    pp.normalize_per_cell(adata_filt)
 
-    sc.pp.log1p(adata)
-    sc.pp.log1p(adata_filt)
+    pp.log1p(adata)
+    pp.log1p(adata_filt)
     if scale:
-        sc.pp.scale(adata_filt)
+        pp.scale(adata_filt)
 
     return adata, adata_filt
 
 
-def dimensionality_reduction(adata_filt, n_comps=50, n_neighbors=10, min_dist=0.5, n_dims=3, metric="correlation",
+def dimensionality_reduction(adata_filt, n_comps=50, n_neighbors=10, min_dist=0.5, metric="correlation",
                              is_aggregation=False):
-    sc.pp.pca(adata_filt, n_comps=n_comps, svd_solver='arpack')
+    pp.pca(adata_filt, n_comps=n_comps, svd_solver="arpack")
+
     if is_aggregation:
-        bbknn.bbknn(adata_filt, metric=metric, n_neighbors=n_neighbors)
+        n_batches = len(adata.obs["batch"].unique())
+        bbknn(
+            adata_filt,
+            metric="angular",
+            neighbors_within_batch=n_neighbors//n_batches,
+            approx=True
+        )
     else:
-        sc.pp.neighbors(adata_filt, n_neighbors=n_neighbors, metric=metric)
-    adata_filt.uns['neighbors']['params']['metric'] = metric
-    sc.tl.umap(adata_filt, min_dist=0.5, n_components=n_dims)
+        neighbors(adata_filt, n_neighbors=n_neighbors, metric=metric)
+
+    adata_filt.uns["neighbors"]["params"]["metric"] = metric
+    umap(adata_filt, min_dist=min_dist, n_components=3)
+    adata_filt.obsm["X_umap_3d"] = adata_filt.obsm["X_umap"].copy()
+    umap(adata_filt, min_dist=min_dist, n_components=2)
 
 
-def cluster(adata_filt):
-    sc.tl.louvain(adata_filt)
-    sc.tl.paga(adata_filt)
-    if adata_filt.obs['louvain'].astype(int).min() == 0:
-        adata_filt.obs['louvain'] = (adata_filt.obs['louvain'].astype(int) + 1).astype('category')
-    #_ = scatter(adata_filt, basis='umap', color='louvain', title='', components='1,2', legend_loc='on data')
-    #_ = scatter(adata_filt, basis='umap', color='louvain', title='', components='1,3', legend_loc='on data')
-    #_ = scatter(adata_filt, basis='umap', color='louvain', title='', components='2,3', legend_loc='on data')
+def cluster(adata_filt, key_added="cluster", use_louvain=False):
+    if use_louvain:
+        louvain(adata_filt, key_added=key_added)
+    else:
+        leiden(adata_filt, key_added=key_added)
+
+    #sc.tl.paga(adata_filt)
+    if adata_filt.obs[key_added].astype(int).min() == 0:
+        adata_filt.obs[key_added] = (adata_filt.obs[key_added].astype(int) + 1).astype("category")
 
 __api_objects__ = {
     "preprocess": preprocess,
@@ -49,23 +66,23 @@ __api_objects__ = {
 
 
 #def find_markers(adata_filt):
-#    sc.tl.rank_genes_groups(adata_filt, 'louvain', method='wilcoxon', use_raw=False,
+#    sc.tl.rank_genes_groups(adata_filt, "louvain", method="wilcoxon", use_raw=False,
 #                            key_added="rank_genes_groups-wilcoxon")
-#    sc.tl.rank_genes_groups(adata_filt, 'louvain', method='t-test_overestim_var', use_raw=False,
+#    sc.tl.rank_genes_groups(adata_filt, "louvain", method="t-test_overestim_var", use_raw=False,
 #                            key_added="rank_genes_groups-ttest")
-#    save_file = os.path.join(OUTPUT_DIRS[adata_filt.uns['sampleid']], 'top5_genes-wilcox.pdf')
-#    fig = sc.pl.rank_genes_groups_matrixplot(adata_filt, groupby='louvain', n_genes=5,
-#                                             key='rank_genes_groups-wilcoxon', use_raw=False,
+#    save_file = os.path.join(OUTPUT_DIRS[adata_filt.uns["sampleid"]], "top5_genes-wilcox.pdf")
+#    fig = sc.pl.rank_genes_groups_matrixplot(adata_filt, groupby="louvain", n_genes=5,
+#                                             key="rank_genes_groups-wilcoxon", use_raw=False,
 #                                             cmap=cmo.cm.tempo,
 #                                             figsize=(36, 6), save=True)
-#    os.rename(os.path.join('figures', 'matrixplot.pdf'), save_file)
+#    os.rename(os.path.join("figures", "matrixplot.pdf"), save_file)
 #
-#    save_file = os.path.join(OUTPUT_DIRS[adata_filt.uns['sampleid']], 'top5_genes-ttest.pdf')
-#    fig = sc.pl.rank_genes_groups_matrixplot(adata_filt, groupby='louvain', n_genes=5,
-#                                             key='rank_genes_groups-ttest', use_raw=False,
+#    save_file = os.path.join(OUTPUT_DIRS[adata_filt.uns["sampleid"]], "top5_genes-ttest.pdf")
+#    fig = sc.pl.rank_genes_groups_matrixplot(adata_filt, groupby="louvain", n_genes=5,
+#                                             key="rank_genes_groups-ttest", use_raw=False,
 #                                             cmap=cmo.cm.tempo,
 #                                             figsize=(36, 6), save=True)
-#    os.rename(os.path.join('figures', 'matrixplot.pdf'), save_file)
+#    os.rename(os.path.join("figures", "matrixplot.pdf"), save_file)
 #
 #
 #def print_markers(adata_filt, method, outfile):
@@ -74,4 +91,4 @@ __api_objects__ = {
 #    marker_df.index.name = "Rank"
 #    marker_df.columns.name = "Cluster ID"
 #
-#    marker_df.to_csv(os.path.join(OUTPUT_DIRS[adata_filt.uns['sampleid']], outfile))
+#    marker_df.to_csv(os.path.join(OUTPUT_DIRS[adata_filt.uns["sampleid"]], outfile))
--- scanpy_recipes/recipes/qc.py
+++ scanpy_recipes/recipes/qc.py
@@ -1,82 +1,124 @@
+import pkg_resources
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 from scanpy import queries
 from scanpy.preprocessing.simple import filter_cells, filter_genes
 
+
+def read_mito_file(genome):
+    return pd.read_csv(pkg_resources.resource_filename("scanpy_recipes",
+                                                       f"data/mito_genes.{genome}.csv"),
+                       index_col=1, header=0).index
+
 def gen_qc(raw_adata):
-    if raw_adata.uns['genome'].lower().startswith('m'):
-        mt_query = queries.mitochondrial_genes('useast.ensembl.org', 'mmusculus')
+    genome = raw_adata.uns["genome"]
+    mt_query = read_mito_file(genome)
+    if genome.lower().startswith("m"):
+        #mt_query = queries.mitochondrial_genes("useast.ensembl.org", "mmusculus")
+        rbc_gene = "Hbb-bs"
     else:
-        mt_query = queries.mitochondrial_genes('useast.ensembl.org', 'hsapiens')
+        #mt_query = queries.mitochondrial_genes("useast.ensembl.org", "hsapiens")
+        rbc_gene = "HBB"
 
     mito_genes = raw_adata.var_names.intersection(mt_query)
-    raw_adata.obs['percent_mito'] = np.sum(
+    raw_adata.obs["percent_mito"] = np.sum(
         raw_adata[:, mito_genes].X, axis=1).A1 / np.sum(raw_adata.X, axis=1).A1 * 100
 
-    raw_adata.obs['n_counts'] = np.sum(raw_adata.X, axis=1).A1
-    raw_adata.obs['n_genes'] = np.sum(raw_adata.X > 0, axis=1).A1
-    raw_adata.var['n_counts'] = np.sum(raw_adata.X, axis=0).A1
-    raw_adata.var['n_cells'] = np.sum(raw_adata.X > 0, axis=0).A1
+    raw_adata.obs["hemoglobin_counts"] = raw_adata[:, rbc_gene].X
+
+    raw_adata.obs["n_counts"] = np.sum(raw_adata.X, axis=1).A1
+    raw_adata.obs["n_genes"] = np.sum(raw_adata.X > 0, axis=1).A1
+    raw_adata.var["n_counts"] = np.sum(raw_adata.X, axis=0).A1
+    raw_adata.var["n_cells"] = np.sum(raw_adata.X > 0, axis=0).A1
 
     raw_cells, raw_genes = raw_adata.shape
-    raw_adata.uns['raw_cells'] = raw_cells
-    raw_adata.uns['raw_genes'] = raw_genes
-    raw_adata.uns['empty_genes'] = np.sum(raw_adata.var['n_cells'] == 0).astype(int)
+    raw_adata.uns["raw_cells"] = raw_cells
+    raw_adata.uns["raw_genes"] = raw_genes
+    raw_adata.uns["empty_genes"] = np.sum(raw_adata.var["n_cells"] == 0).astype(int)
+
+    raw_adata.uns["obs_titles"] = dict(
+        n_counts="UMIs", n_genes="Genes", percent_mito="mtRNA content",
+        sequencing_saturation="Sequencing saturation",
+        hemoglobin_counts="Hemoglobin counts"
+    )
 
 
-def run_qc(adata, min_cells=3, min_genes=200, min_counts_per_cell=500, min_counts_per_gene=3,
-           sequencing_saturation=50.0, percent_mito=50.0):
+def run_qc(adata_raw,
+           min_cells_per_gene=3,
+           min_counts_per_gene=3,
+           min_genes_per_cell=200,
+           min_counts_per_cell=500,
+           sequencing_saturation=50.0,
+           percent_mito=50.0,
+           rbc_threshold=10,
+           trial=False):
+    """
 
-    orig_shape = adata.shape
-    filter_genes(adata, min_cells=min_cells)
-    filter_genes(adata, min_counts=min_counts_per_gene)
-    filter_cells(adata, min_counts=min_counts_per_cell)
-    filter_cells(adata, min_genes=min_genes)
+    """
+    orig_shape = adata_raw.shape
+    adata = adata_raw.copy()
+    adata_qc = adata_raw.copy()
 
-    redux = adata.copy()
-    redux = adata[adata.obs['sequencing_saturation'] > sequencing_saturation]
-    redux = redux[redux.obs['percent_mito'] < percent_mito]
-    qc_shape = redux.shape
+    filter_genes(adata_qc, min_cells=min_cells_per_gene)
+    filter_genes(adata_qc, min_counts=min_counts_per_gene)
+    adata.var["qc_fail_counts"] = ~adata.var_names.isin(adata_qc.var_names)
+    filter_cells(adata_qc, min_counts=min_counts_per_cell)
+    filter_cells(adata_qc, min_genes=min_genes_per_cell)
+    adata.obs["qc_fail_counts"] = ~adata.obs_names.isin(adata_qc.obs_names)
+
+    seqsat_subset, mito_subset, rbc_subset = True, True, True
+    if sequencing_saturation:
+        seqsat_subset = adata_qc.obs["sequencing_saturation"] > sequencing_saturation
+    if percent_mito:
+        mito_subset = adata_qc.obs["percent_mito"] < percent_mito
+    if rbc_threshold:
+        rbc_subset = adata_qc.obs["hemoglobin_counts"] < rbc_threshold
+    keep_subset = seqsat_subset & mito_subset & rbc_subset
+    adata_qc._inplace_subset_obs(keep_subset)
+
+    adata.obs["qc_fail_seqsat"] = ~seqsat_subset
+    adata.obs["qc_fail_mito"] = ~mito_subset
+    adata.obs["qc_fail_rbc"] = ~rbc_subset
+    adata.obs["qc_fail"] = "fail"
+    adata.obs.loc[keep_subset, "qc_fail"] = "pass"
+    adata.obs["qc_fail"] = adata.obs["qc_fail"].astype("category")
+
+    n_rbcs = int(sum(~rbc_subset))
+    adata.uns["qc_gene_filter"] = {
+        "threshold_n_cells": min_cells_per_gene,
+        "threshold_n_counts": min_counts_per_gene
+    }
+    adata.uns["qc_cell_filter"] = {
+        "threshold_n_genes": min_genes_per_cell,
+        "threshold_n_counts": min_counts_per_cell,
+        "threshold_sequencing_saturation": sequencing_saturation,
+        "threshold_percent_mito": percent_mito,
+        "threshold_hemoglobin_counts": rbc_threshold
+    }
+
+    qc_metrics = {
+        "red_blood_cells_removed": n_rbcs,
+        "cells_removed": int(sum(adata.obs.qc_fail == "fail")),
+        "high_mtrna_cells_removed": int(sum(~mito_subset & rbc_subset)),
+        "low_sequencing_saturation_cells_removed": \
+                int(sum(~seqsat_subset & rbc_subset & mito_subset)),
+        "low_count_cells_removed": int(sum(adata.obs.qc_fail_counts)),
+        "low_count_genes_removed": int(sum(adata.var.qc_fail_counts)),
+        "cells": adata_qc.shape[0],
+        "genes": adata_qc.shape[1]
+    }
+    adata.uns["qc_metrics"] = qc_metrics
+
+    qc_shape = adata_qc.shape
     print("Original dims: {}\nFiltered dims: {}".format(orig_shape, qc_shape))
 
-    redux.uns = adata.uns.copy()
-    redux.uns['qc_gene_filter'] = {'min_cells': min_cells, 'min_counts': min_counts_per_gene}
-    redux.uns['qc_cell_filter'] = {'min_genes': min_genes, 'min_counts': min_counts_per_cell,
-                                   'min_sequencing_saturation': sequencing_saturation,
-                                   'max_percent_mito': percent_mito}
+    if trial:
+        return adata
 
-    return redux
-
-
-def detect_rbc(adata, threshold=10):
-    if adata.uns["species"] == 'mmusculus':
-        key_gene = 'Hbb-bs'
-    else:
-        key_gene = 'HBB'
-
-    cell_subset = adata[:, key_gene].X < threshold
-    n_rbcs = adata.shape[0] - sum(cell_subset)
-
-    fig, (ax1, ax2) = plt.subplots(1, 2, dpi=300, figsize=(8, 3))
-    fig.patch.set_facecolor("white")
-    p1 = ax1.scatter(adata.obs['n_counts'], adata.obs['n_genes'], c=adata[:, key_gene].X, cmap='Reds', s=12, alpha=0.8)
-    ax1.set_xlabel('UMIs')
-    ax1.set_ylabel('Genes')
-    ax1.set_title('Before filtering')
-    fig.colorbar(p1, ax=ax1, label=key_gene)
-
-
-    adata._inplace_subset_obs(cell_subset)
-    adata.uns['red_blood_cells_removed'] = n_rbcs
-    p2 = ax2.scatter(adata.obs['n_counts'], adata.obs['n_genes'], c=adata[:, key_gene].X, cmap='Reds', s=12, alpha=0.8)
-    ax2.set_xlabel('UMIs')
-    ax2.set_ylabel('Genes')
-    ax2.set_title('After filtering')
-    fig.colorbar(p2, ax=ax2, label=key_gene)
-    fig.tight_layout()
-
-    print(f"Removed {n_rbcs} red blood cells.")
+    adata_qc.uns = adata.uns.copy()
+    return adata_qc
 
 
 __api_objects__ = {
