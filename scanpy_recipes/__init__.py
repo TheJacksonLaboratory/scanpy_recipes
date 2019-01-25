52c8f36db01baa441ab8c59e0ae1edfb4abff9ce
Bill Flynn <wflynny@gmail.com>
2019-01-25 15:59:28 -0500

    move away from '.api' #10

--- scanpy_recipes/__init__.py
+++ scanpy_recipes/__init__.py
@@ -3,3 +3,46 @@
 #__version__ = get_versions()['version']
 #del get_versions
 __version__ = "0.1"
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import scanpy as sc
+from .read_write import __api_objects__ as read_write_api
+from .report import __api_objects__ as report_api
+from .recipes import aggregate, preprocess, qc, tools
+from .plotting import qc as plqc
+from .plotting.rcmod import update_figure_params
+
+for object_key, object_ in read_write_api.items():
+    sc.__dict__[object_key] = object_
+
+for object_key, object_ in report_api.items():
+    sc.__dict__[object_key] = object_
+
+for object_key, object_ in aggregate.__api_objects__.items():
+    sc.__dict__[object_key] = object_
+
+#sc.processing = preprocess
+sc.qc = qc
+
+for object_key, object_ in qc.__api_objects__.items():
+    sc.qc.__dict__[object_key] = object_
+
+for object_key, object_ in preprocess.__api_objects__.items():
+    sc.pp.__dict__[object_key] = object_
+
+for object_key, object_ in tools.__api_objects__.items():
+    sc.tl.__dict__[object_key] = object_
+
+for object_key, object_ in plqc.__api_objects__.items():
+    sc.pl.__dict__[object_key] = object_
+
+update_figure_params()
+
+sc.settings.verbosity = 3
+
+del read_write_api, report_api
+del preprocess, qc, tools, plqc
+
+sc.logging.print_version_and_date()
